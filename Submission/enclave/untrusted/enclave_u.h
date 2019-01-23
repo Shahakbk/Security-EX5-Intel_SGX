9161a6ba78fd156b204fe21ba5229481fc4e5d9b
Shahakbk <shahakbk@gmail.com>
2019-01-23 16:21:30 +0200

    added EDL, LDS and Bob & Alice makefiles

--- Submission/enclave/untrusted/enclave_u.h
+++ Submission/enclave/untrusted/enclave_u.h
@@ -27,8 +27,8 @@ void SGX_UBRIDGE(SGX_NOCONVENTION, ocall_enclave_sample, (const char* str));
 /** ECalls **/
 
 sgx_status_t ecall_enclave_write(sgx_enclave_id_t eid, int *retval, unsigned int *data, unsigned int num_customers);
-sgx_status_t ecall_enclave_generate_keys(sgx_enclave_id_t eid, int *retval, sgx_ec256_public_t *encryption_key, sgx_ec256_public_t *signature_key);
-sgx_status_t ecall_enclave_generate_shared_key(sgx_enclave_id_t eid, int *retval, sgx_ec256_public_t *encryption_key, sgx_ec256_public_t *signature_key);
+sgx_status_t ecall_enclave_generate_keys(sgx_enclave_id_t eid, int *retval, sgx_ec256_public_t *public_encryption_key, sgx_ec256_public_t *public_signature_key);
+sgx_status_t ecall_enclave_generate_shared_key(sgx_enclave_id_t eid, int *retval, sgx_ec256_public_t *received_public_encryption_key, sgx_ec256_public_t *received_public_signature_key);
 sgx_status_t ecall_enclave_encrypt_data(sgx_enclave_id_t eid, int *retval, unsigned int *encrypted_data, unsigned int *encrypted_num_customers, sgx_ec256_signature_t *data_signature);
 sgx_status_t ecall_enclave_write_encrypted(sgx_enclave_id_t eid, int *retval, unsigned int *encrypted_data, unsigned int *encrypted_num_customers, sgx_ec256_signature_t data_signature);
 sgx_status_t ecall_enclave_decrypt_data(sgx_enclave_id_t eid, int *retval);
